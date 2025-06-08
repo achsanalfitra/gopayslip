347cb1e5b8afbf22c0ac0b24c3a3c8daadf52092
Alfitra Achsan <achsanalfitra@gmail.com>
2025-06-08 16:53:35 +0700

    feat(server.go): add Start() function that starts the server and implement a const that handle server config

--- cmd/config/server.go
+++ cmd/config/server.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"log"
 	"net/http"
+	"time"
 )
 
 type Server struct {
@@ -9,6 +11,12 @@ type Server struct {
 	Router *http.ServeMux
 	Srv    *http.Server
 }
+
+const (
+	ReadTimeout    = 10 * time.Second
+	WriteTimeout   = 10 * time.Second
+	MaxHeaderBytes = 1 << 20
+)
 
 // custom server building
 func CreateServer(addr string, rtr *http.ServeMux) *Server {
@@ -18,9 +26,23 @@ func CreateServer(addr string, rtr *http.ServeMux) *Server {
 	}
 
 	srv.Srv = &http.Server{
-		Addr:    srv.Addr,
-		Handler: srv.Router,
+		Addr:           srv.Addr,
+		Handler:        srv.Router,
+		ReadTimeout:    ReadTimeout,
+		WriteTimeout:   WriteTimeout,
+		MaxHeaderBytes: MaxHeaderBytes,
 	}
 
 	return &srv
+}
+
+func (s *Server) Start() {
+	log.Printf("running server on port: %s", s.Addr)
+
+	// do error check when the server fails to run
+	if err := s.Srv.ListenAndServe(); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
+
+	log.Print("server is running")
 }
