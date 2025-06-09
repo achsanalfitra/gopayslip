9821b3e1f254393e567acfdbe17725d020be5a97
Alfitra Achsan <achsanalfitra@gmail.com>
2025-06-09 14:08:23 +0700

    refactor(auth/): 1. split authservices and authhandlers 2. remove token logic from authservices and authhandlres 3. change QueryRow into QueryRowContext

---
+++ internal/auth/authhandlers.go
@@ -0,0 +1,8 @@
+package auth
+
+// type User struct {
+// 	ID       int    `json:"id"`
+// 	Username string `json:"user"`
+// 	Password string `json:"pass"`
+// 	Role     string `json:"role"`
+// }
---
+++ internal/auth/authservices.go
@@ -0,0 +1,64 @@
+package auth
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/achsanalfitra/gopayslip/internal/app"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService interface {
+	Login(user, pass, role string, ctx context.Context) error
+	Register(user, pass, role string, ctx context.Context) error
+}
+
+func Login(user, pass, role string, ctx context.Context) error {
+	var hashedPassword string
+
+	db := app.GetDB(ctx)
+
+	err := db.QueryRowContext(ctx, "SELECT password FROM users WHERE username=$1 and role=$2", user, role).Scan(&hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return errors.New("user not found")
+		}
+		return err
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(pass)); err != nil {
+		return errors.New("invalid password")
+	}
+
+	return nil
+}
+
+func Register(user, pass, role string, ctx context.Context) error {
+	db := app.GetDB(ctx)
+
+	// check if user already exists, this early exit increases performance
+	var existingUser string
+	err := db.QueryRowContext(ctx, "SELECT username FROM users WHERE username=$1 and role=$2", user, role).Scan(&existingUser)
+	if err == nil {
+		return errors.New("user already exists")
+	}
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return err
+	}
+
+	hashedPasswordBytes, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	hashedPassword := string(hashedPasswordBytes)
+
+	_, err = db.ExecContext(ctx, "INSERT INTO users (username, password, role) VALUES ($1, $2, $3)", user, hashedPassword, role)
+	if err != nil {
+		return errors.New("failed to register user")
+	}
+
+	return nil
+}
