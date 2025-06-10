72c203b5b3571950efad3b85c2310cf79c9bfa83
Alfitra Achsan <achsanalfitra@gmail.com>
2025-06-10 20:34:45 +0700

    feat(reimbursement.go): create a user model within /model/reimbursement.go

---
+++ internal/model/reimbursment.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Reimbursement struct {
+	ID                  int64     `json:"id"`
+	UserID              int64     `json:"user_id"`
+	CreatedBy           int64     `json:"created_by"`
+	UpdatedBy           int64     `json:"updated_by"`
+	ReimbursementAmount float64   `json:"reimbursement_amount"`
+	RequestId           uuid.UUID `json:"request_id"`
+	UserRole            Role      `json:"user_role"`
+	Description         string    `json:"description"`
+	CreatedAt           time.Time `json:"created_at"`
+	UpdatedAt           time.Time `json:"updated_at"`
+}
