7e9dee43fe74eec1af0a92d099cac15c7f9347d0
Alfitra Achsan <achsanalfitra@gmail.com>
2025-06-10 20:37:45 +0700

    feat(overtime.go): create a user model within /model/overtime.go

---
+++ internal/model/overtime.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Overtime struct {
+	ID        int64         `json:"id"`
+	UserID    int64         `json:"user_id"`
+	CreatedBy int64         `json:"created_by"`
+	UpdatedBy int64         `json:"updated_by"`
+	RequestId uuid.UUID     `json:"request_id"`
+	UserRole  Role          `json:"user_role"`
+	Interval  time.Duration `json:"duration"`
+	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
+}
