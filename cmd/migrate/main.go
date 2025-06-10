f136d591df774aae3b92842cabcc38553ced9fb9
Alfitra Achsan <achsanalfitra@gmail.com>
2025-06-10 15:10:31 +0700

    feat(cmd/migrate/main.go): implement the new migration setup along with its init schema

---
+++ cmd/migrate/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/achsanalfitra/gopayslip/internal/config"
+	"github.com/achsanalfitra/gopayslip/internal/migration"
+)
+
+func main() {
+	db, err := config.InitDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// for demo project, we will only demosntrate the UP function
+	m := migration.NewMigration(
+		string(migration.UP),
+		"internal/migration/migrations/",
+		"schema_migration.sql",
+		db.DB,
+	)
+
+	if err := m.InitMigrationSchema(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
+	}
+
+	db.DB.Close()
+}
